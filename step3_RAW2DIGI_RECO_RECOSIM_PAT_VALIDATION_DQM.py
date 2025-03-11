00631e9877504d8ab755454eafa3f35ec35716e5
bainbrid <bainbrid@users.noreply.github.com>
2025-03-11 15:48:49 +0100

    add TICLDumper

--- step3_RAW2DIGI_RECO_RECOSIM_PAT_VALIDATION_DQM.py
+++ step3_RAW2DIGI_RECO_RECOSIM_PAT_VALIDATION_DQM.py
@@ -291,3 +291,6 @@ process = customiseLogErrorHarvesterUsingOutputCommands(process)
 from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
 process = customiseEarlyDelete(process)
 # End adding early deletion
+
+from RecoHGCal.TICL.customiseTICLFromReco import customiseTICLForDumper
+process = customiseTICLForDumper(process)
